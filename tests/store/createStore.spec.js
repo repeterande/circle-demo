71ef47c27677812daf75faeb83ccd4635c8eeb93
Peter Andersson <peter.andersson@evry.com>
2018-04-12 07:58:27 +0200

    fixed broken test

--- tests/store/createStore.spec.js
+++ tests/store/createStore.spec.js
@@ -23,7 +23,7 @@ describe('(Store) createStore', () => {
         type    : 'LOCATION_CHANGE',
         payload : location
       })
-      expect(store.getState().location).to.not.deep.equal(location)
+      expect(store.getState().location).to.deep.equal(location)
     })
   })
 })
